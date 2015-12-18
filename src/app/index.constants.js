fc5012d80f35dc10d07e3f8ce7f4dd1179394e3e
Tomás Fox <tomas.c.fox@gmail.com>
2015-12-18 17:10:40 -0300

    Initial commit

---
+++ src/app/index.constants.js
@@ -0,0 +1,10 @@
+/* global malarkey:false, moment:false */
+(function() {
+  'use strict';
+
+  angular
+    .module('addressBookExercise')
+    .constant('malarkey', malarkey)
+    .constant('moment', moment);
+
+})();
